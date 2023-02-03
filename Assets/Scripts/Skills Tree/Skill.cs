d66be32e2e00e066d7e9b128f86fc2eee3dc0e71
linus <linus@zyax.se>
2023-02-03 20:27:57 +0100

    Began work on skill tree and skill system

--- Assets/Scripts/Skills Tree/Skill.cs
+++ Assets/Scripts/Skills Tree/Skill.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class Skill : ScriptableObject
+namespace Skills
 {
+    [CreateAssetMenu(fileName = "Skill", menuName = "Skill Tree/New Skill", order = 1)]
+    public class Skill : ScriptableObject
+    {
+        [Header("Skill Options")]
+        public string Name;
+        public int PowerLevel; //Used for random generation of skill tree
 
+        [Space, TextArea] public string Description;
+
+        [Space, Header("Usage Promtps")]
+        public List<string> SkillUsagePrompts = new();
+
+        [Header("Stats")]
+        public Stat Stat;
+    }
 }
--- Assets/Scripts/Skills Tree/SkillManager.cs
+++ Assets/Scripts/Skills Tree/SkillManager.cs
@@ -1,18 +1,63 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class SkillManager : MonoBehaviour
+namespace Skills
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Attribute
     {
-        
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Luck
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class Stat
     {
-        
+        [Header("Stat Options")]
+        public string name;
+
+        [Space] public Attribute attribute;
+
+        public Stat(string name, Attribute attribute, float modifier)
+        {
+            this.name = name;
+            this.attribute = attribute;
+            Modifier = modifier;
+        }
+
+        [Space, Range(-100f, 100f)]
+        public float Modifier;
+    }
+
+    public class SkillManager : MonoBehaviour
+    {
+        private List<Skill> m_skills = new();
+
+        public static SkillManager instance;
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            //"Singleton"
+            if (instance != this)
+                instance = this;
+        }
+
+        #endregion Unity Methods
+
+        public void AddSkill(Skill skill) => m_skills.Add(skill);
+
+        public float GetAttributeModifier(Attribute attribute)
+        {
+            var total = 0f;
+            var attributes = m_skills.Where(x => x.Stat.attribute == attribute).ToList();
+            attributes.ForEach(skill => { if (skill.Stat.attribute == attribute) total += skill.Stat.Modifier; });
+            return total;
+        }
     }
 }
