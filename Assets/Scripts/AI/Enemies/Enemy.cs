2625a991c987699fbe523937a74495e04f61adb0
Lars Modig <lars@brocc.se>
2023-02-04 21:37:46 +0100

    EnemyStat List added

--- Assets/Scripts/AI/Enemies/Enemy.cs
+++ Assets/Scripts/AI/Enemies/Enemy.cs
@@ -1,6 +1,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyAttribute
+{
+    Thorny,
+    Poisonous,
+    Lust,
+    Size
+}
+
+[System.Serializable]
+public class EnemyStat
+{
+    [Header("Stat Options")]
+    public string name;
+
+    [Space] public EnemyAttribute attribute;
+
+    public EnemyStat(string name, EnemyAttribute attribute, float modifier)
+    {
+        this.name = name;
+        this.attribute = attribute;
+        Modifier = modifier;
+    }
+
+    [Space, Range(-100f, 100f)]
+    public float Modifier;
+}
+
 [System.Serializable]
 public class EnemyAttack
 {
@@ -25,6 +52,9 @@ public class Enemy : ScriptableObject
 
     [Space]
     public Vector2 XPDropRange = new();
+
+    [Space]
+    public List<EnemyStat> Stats = new();
 
     [Space]
     public List<Item> DropTable = new();
