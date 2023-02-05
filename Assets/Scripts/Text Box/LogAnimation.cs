dec6acc1bb9ff974e1b91f790edbed6dc1565d7f
Samuli <samuli.jaaskelainen@varjo.com>
2023-02-05 09:53:53 +0200

    Add log animation and audio.

---
+++ Assets/Scripts/Text Box/LogAnimation.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogAnimation : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Vector3 localHoverPos;
+    Vector3 normalPos;
+    bool droppingDown = false;
+
+    void Start()
+    {
+        normalPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+        Debug.Log(Screen.height);
+        if (Input.mousePosition.y > Screen.height * 0.8f)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, localHoverPos, Time.deltaTime * speed);
+
+            if(!droppingDown)
+            {
+                droppingDown = true;
+                AudioManager.Instance.PlaySound("SkillDescriptionVariation1");
+            }
+        }
+        else
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, normalPos, Time.deltaTime * speed);
+
+            if (droppingDown)
+            {
+                droppingDown = false;
+            }
+        }
+    }
+}
